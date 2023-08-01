328054f16c48b5b9d10d1c7cd3416557e3dc1bd7
Eduardo <132105117+3DUTEX@users.noreply.github.com>
2023-08-01 01:39:38 -0300

    autenticação alterada e calculo imc feito

--- main.js
+++ main.js
@@ -8,36 +8,51 @@ const mensagem = document.querySelector("#mensagem");
 const btnCalc = document.querySelector("#btnCalc");
 const btnLimp = document.querySelector("#btnLimp");
 
-const checkFill = () => {
+//checando o tipo dos valores e se os inputs estão preenchidos
+const auth = () => {
   if (inputPeso.value === "") {
     alert("Preencha com o seu peso!");
+    inputPeso.focus();
+  } else if (isNaN(inputPeso.value)) {
+    alert("Preencha o peso apenas com números!");
     inputPeso.focus();
   } else if (inputAlt.value === "") {
     alert("Preencha com a sua altura!");
     inputAlt.focus();
-  } else if (!rdbF.checked) {
-    if (!rdbM.checked) {
-      alert("Preencha o seu sexo!");
+  } else if (isNaN(inputAlt.value)) {
+    alert("Preencha a altura apenas com números!");
+    inputAlt.focus();
+  } else if (!rdbF.checked && !rdbM.checked) {
+    alert("Preencha com o seu sexo!");
+  } else {
+    btnCalc.focus();
+    return true;
+  }
+};
+
+//função que define mensagem dependendo do imc e do sexo
+const messagePerImc = (imc) => {
+  if (rdbF.checked) {
+  } else {
+    if (imc < 19) {
+      mensagem.textContent = "Abaixo do peso";
     }
   }
 };
 
-const checkType = () => {
-  //verifica se o valor não é um número,se não for retorna true e atende a condição
-  if (isNaN(inputPeso.value)) {
-    alert("Preencha o peso apenas com números!");
-  } else if (isNaN(inputAlt.value)) {
-    alert("Preencha a altura apenas com números!");
-  }
-};
-
+//evento de click no botão calcular
 btnCalc.addEventListener("click", (e) => {
   //removendo função padrão do botão
   e.preventDefault();
-  checkFill();
-  checkType();
+  if (auth()) {
+    const imc = inputPeso.value / (inputAlt.value * inputAlt.value);
+    resultado.textContent = `Seu IMC é de: ${imc.toFixed(2)}`;
+
+    messagePerImc(imc);
+  }
 });
 
+//limpa o forms
 btnLimp.addEventListener("click", (e) => {
   e.preventDefault();
 
